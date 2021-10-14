b5d54a2efc004448e35360507d00666b5c59747c
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-14 17:23:46 +0200

    slideshow funzionante, da completare

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -30,8 +30,8 @@ for (let i = 0; i < items.length; i++) {
     const element = items[i];
     const img = document.createElement('img');
     img.src = element;
-    img.alt = `image #${i}`;
-    img.id = `image #${i}`;
+    img.alt = `image-${i}`;
+    img.id = `image-${i}`;
     console.log(img.src);
     menuContainer.appendChild(img); 
 }
@@ -39,5 +39,42 @@ for (let i = 0; i < items.length; i++) {
 const activeImage = document.createElement('img')
 activeImage.src = items[1];
 activeContainer.appendChild(activeImage);
-const activeMenuImage = document.getElementById('image #1');
+const activeMenuImage = document.getElementById('image-1');
 activeMenuImage.setAttribute('class', 'active-menu-image');
+
+
+const chevronUp = document.querySelector('.fa-chevron-up');
+const chevronDown = document.querySelector('.fa-chevron-down');
+
+chevronUp.addEventListener('click', function(){
+    //immagine attualmente nel container principale
+    const currentActive = document.querySelector('.active-menu-image');
+    //estrapolo numero dall'id dell'attuale immagine principale
+    let currentIndex = parseInt(currentActive.id.replace( /^\D+/g, ''));
+    //aumento l'indice di 1
+    currentIndex--;
+    if(currentIndex < 0){
+        currentIndex = 4;
+    }
+    //riassegno l'src col nuovo src del nuovo indice
+    currentActive.classList.remove('active-menu-image');
+    const newActive = document.getElementById(`image-${currentIndex}`);
+    newActive.setAttribute('class', 'active-menu-image');
+    activeImage.src = items[currentIndex];
+})
+chevronDown.addEventListener('click', function(){
+    //immagine attualmente nel container principale
+    const currentActive = document.querySelector('.active-menu-image');
+    //estrapolo numero dall'id dell'attuale immagine principale
+    let currentIndex = parseInt(currentActive.id.replace( /^\D+/g, ''));
+    //aumento l'indice di 1
+    currentIndex++;
+    if(currentIndex === items.length){
+        currentIndex = 0;       
+    }
+    //riassegno l'src col nuovo src del nuovo indice
+    currentActive.classList.remove('active-menu-image');
+    const newActive = document.getElementById(`image-${currentIndex}`);
+    newActive.setAttribute('class', 'active-menu-image');
+    activeImage.src = items[currentIndex];
+})
